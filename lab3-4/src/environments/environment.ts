42e053625853803c9b75016e06a664aebfe236b4
xenoteo <44746889+xenoteo@users.noreply.github.com>
2020-06-10 21:58:43 +0200

    Update environment.ts

--- lab3-4/src/environments/environment.ts
+++ lab3-4/src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyB_YTjniY9BgeJ8pbOV1qpIAPgm61B7roA",
-    authDomain: "bicycles-and-scooters.firebaseapp.com",
-    databaseURL: "https://bicycles-and-scooters.firebaseio.com",
-    projectId: "bicycles-and-scooters",
-    storageBucket: "bicycles-and-scooters.appspot.com",
-    messagingSenderId: "5805640152",
-    appId: "1:5805640152:web:b2ac9f64ae7c3e25d94aad",
-    measurementId: "G-LSNQV9088J"
-  }
+  production: false
 };
 
 /*
